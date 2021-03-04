806263c9c07a9ee91de346aedeee37165dc11b2d
cstutor-swe <74371276+xpqx@users.noreply.github.com>
2021-03-04 11:40:02 -0800

    Create WomenWhoCodeQuiz1.java

---
+++ WomenWhoCode/quiz_1/files/WomenWhoCodeQuiz1.java
@@ -0,0 +1,20 @@
+package womenWhoCode.java.quiz1;
+
+public class WomenWhoCodeQuiz1 {
+	
+	public static void main(String[] args) {
+		
+		String fruit = "banana";
+		int length = fruit.length();
+		System.out.println(fruit);
+		System.out.println(length);
+		
+	}
+
+}
+
+/****
+*OUTPUT
+*banana
+*6
+*/
