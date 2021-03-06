44d4e1e4705dd4608e6890f295abeb6ecdf266c7
cstutor-swe <74371276+xpqx@users.noreply.github.com>
2021-03-05 18:06:17 -0800

    Create Quiz2.java

---
+++ WomenWhoCode/quiz_2/Quiz2.java
@@ -0,0 +1,22 @@
+package womenWhoCode.quizzes;
+
+public class Quiz2 {
+	public static void main(String[] args) {
+		int num = 3;
+		int num1 = 8;
+		int num2 = 4;
+		int result = (num * (8 + 4));
+		String res = "Result: ";
+		System.out.println(res + result);
+	}
+
+}
+/*
+OUTPUT
+Result: 36
+
+
+
+
+*/
+
