2b1d78351f92ef3b6c6573a081276fe4d545bb7e
cstutor-swe <74371276+xpqx@users.noreply.github.com>
2021-03-13 19:21:57 -0800

    random twitter quiz

---
+++ randomTwitterQuiz1.java
@@ -0,0 +1,21 @@
+package randomTwitterQuiz;
+
+public class JavaProblemZeroSumCountZero {
+	public static void main(String[] args) {
+		System.out.println("From a tweet");
+		//there are the only requirements given:
+    //In Java, if the sum is zero, and the count is zero, what's the average?
+    //the average can technically be anything given the prompt
+		
+		int sum = 0;
+		int count = 0;
+		int average = 0;
+		
+		System.out.println(sum + count );
+		System.out.println();
+		System.out.println(average);
+	}
+
+}
+
+
